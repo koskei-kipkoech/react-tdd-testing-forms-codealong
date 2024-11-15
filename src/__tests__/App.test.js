f4a653464d58771ae7afd3ed6aba02680d68e9d2
koskei-kipkoech <patrickwayy@gmail.com>
2024-11-15 22:10:15 +0300

    updates src/test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -37,9 +37,39 @@ test("checkbox appears as unchecked when user clicks a second time", () => {
 });
 
 // Size select element
+test("size select element initially displays 'Small'", () => {
+  render(<App />);
 
+  const selectSize = screen.getByLabelText(/select size/i);
+
+  expect(selectSize).toHaveDisplayValue("Small");
+});
 // "Your Selection" text
+test("selecting options updates the 'Your selection' message", () => {
+  render(<App />);
 
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+  const selectSize = screen.getByLabelText(/select size/i);
+
+  userEvent.click(addPepperoni);
+
+  expect(screen.getByText(/small pepperoni/i)).toBeInTheDocument();
+
+  userEvent.selectOptions(selectSize, "large");
+
+  expect(screen.getByText(/large pepperoni/i)).toBeInTheDocument();
+});
 // "Contact Info" text box
+test("'Contact Info' text box initially displays a placeholder value of 'email address'", () => {
+  render(<App />);
 
+  expect(screen.getByPlaceholderText(/email address/i)).toBeInTheDocument();
+});
 // Submit Order button
+test("form contains a 'Submit Order' button", () => {
+  render(<App />);
+
+  expect(
+    screen.getByRole("button", { name: /submit order/i })
+  ).toBeInTheDocument();
+});
